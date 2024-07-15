ad08c2c35da81036c4a7f2ba630927885c929ce6
MartaKMB <muszkamarta@interia.pl>
2024-07-15 09:55:30 +0200

    clean up for lesson 4

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-// const requestLogger = (config) => {
-//   console.log(
-//     `Zapytanie ${config.method} pod ${config.url} zarejestrowano o ${new Date().toISOString()}`
-//   );
-//   return config;
-// }
-
-// axios.interceptors.request.use(requestLogger);
-
-// Add a request interceptor
 axios.interceptors.request.use(function (config) {
   console.log(
     `Zapytanie ${config.method} pod ${config.url} zarejestrowano o ${new Date().toISOString()}`
@@ -17,9 +7,7 @@ axios.interceptors.request.use(function (config) {
   return config;
 });
 
-// Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
   console.log(`zapytanie zakończono o ${new Date().toISOString()}`)
   return response;
 });
