0c915e7bcc22fcafec390eb02a9c71463ed140ba
MartaKMB <muszkamarta@interia.pl>
2024-07-15 09:54:37 +0200

    solution for lesson 4 2nd part - mmb suggestion

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 
+// const requestLogger = (config) => {
+//   console.log(
+//     `Zapytanie ${config.method} pod ${config.url} zarejestrowano o ${new Date().toISOString()}`
+//   );
+//   return config;
+// }
+
+// axios.interceptors.request.use(requestLogger);
+
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  console.log(
+    `Zapytanie ${config.method} pod ${config.url} zarejestrowano o ${new Date().toISOString()}`
+  );
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
   // Do something with response data
+  console.log(`zapytanie zakończono o ${new Date().toISOString()}`)
   return response;
 });
 
