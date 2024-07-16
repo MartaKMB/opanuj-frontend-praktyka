b422c85c2c32a72af6e8e6f8dbc87c03b4e2a8c5
MartaKMB <muszkamarta@interia.pl>
2024-07-16 16:36:02 +0200

    solution for first exercise lesson 1 module 2 - mmb suggestion

--- examples/module2/lesson1/discount-validator/validator.test.ts
+++ examples/module2/lesson1/discount-validator/validator.test.ts
@@ -12,8 +12,27 @@ describe('Form validation', () => {
         expect(errors).toContain('Last name is required');
     });
 
+    test('should return an error if first name is shorter then one sign', () => {
+        const errors = formValidator('J', '', 30);
+        expect(errors).toContain('Name required at least one sign');
+    });
+
+    test('should return an error if last name is shorter then one sign', () => {
+        const errors = formValidator('', 'D', 30);
+        expect(errors).toContain('Name required at least one sign');
+    });
+
     test('should return an error if age is negative', () => {
         const errors = formValidator('John', 'Doe', -1);
         expect(errors).toContain('Age must be a positive number');
+    });
+
+    test('should return an error if age is not a number', () => {
+        expect(() => formValidator('John', 'Doe', NaN)).toThrowError(/^Error: age must be a number$/);
+    });
+
+    test('should return no error if all data are correct', () => {
+        const data = formValidator('John', 'Doe', 23);
+        expect(data).toStrictEqual([]);
     });
 });
--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -13,6 +13,14 @@ export function formValidator(
     errors.push('Last name is required');
   }
 
+  if (firstName.length < 1 || lastName.length < 1) {
+    errors.push('Name required at least one sign');
+  }
+  
+  if (isNaN(age)) {
+    throw new Error('Error: age must be a number');
+  }
+
   if (age < 0) {
     errors.push('Age must be a positive number');
   }
